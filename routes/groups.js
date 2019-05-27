f300e97e0b0d5b9f7d17d6dd169ade197f044ac5
RedFoxFinn <laborantti.antti@gmail.com>
2019-05-27 17:21:30 +0300

    back: added capability to update groups @ v1.8.0

--- routes/groups.js
+++ routes/groups.js
@@ -27,6 +27,17 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/:addedBy', async (req, res) => {
+  const body = req.body;
+  try {
+    const Groups = await TgGroup.find({addedBy: body.addedBy});
+    Groups.sort((a, b) => (a.name > b.name) ? 1 : -1);
+    res.json(Groups);
+  } catch (e) {
+    res.status(404).send(e, 'couldn\'t find groups').end();
+  }
+});
+
 // POST-route
 router.post('/', async (req, res) => {
   const body = req.body;
@@ -56,6 +67,48 @@ router.post('/', async (req, res) => {
     
   } catch (e) {
     res.status(400).send({error: 'couldn\'t add group'});
+  }
+});
+
+// DELETE-route
+router.delete('/:name', async (req, res) => {
+  const body = req.body;
+  try {
+    const enlUser = EnlUser.findOne({userName: body.userName});
+    const validationCorrect = enlUser === null ? false : bcrypt.compare(body.userValidation, enlUser.userValidation);
+    
+    if (!(enlUser && validationCorrect)) {
+      return res.status(401).json({error: 'insufficient security clearance'});
+    }
+    
+    await TgGroup.findOneAndDelete({name: body.name});
+    res.status(204).end();
+  } catch (e) {
+    res.status(400).end();
+  }
+});
+
+// PUT-route
+router.put('/:name', async (req, res) => {
+  const body = req.body;
+  try {
+    const enlUser = EnlUser.findOne({userName: body.userName});
+    const validationCorrect = enlUser === null ? false : bcrypt.compare(body.userValidation, enlUser.userValidation);
+    
+    if (!(enlUser && validationCorrect)) {
+      return res.status(401).json({error: 'insufficient security clearance'});
+    }
+    
+    await TgGroup.findByIdAndUpdate(
+      {name: body.name},
+      { $set: {name: body.name, sheriff: body.sheriff, link: body.link, info: body.info, linkDateTime: body.linkDateTime, linkExpDateTime: body.linkExpDateTime, addedBy: enlUser.userName}});
+    
+    const groups = await TgGroup.find({});
+    groups.sort((a, b) => (a.name > b.name) ? 1 : -1);
+    
+    res.status(202).json(groups);
+  } catch (e) {
+    res.status(400).end();
   }
 });
 
